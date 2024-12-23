be00cba7ec8648c516ab506a0ae8f29598f258b0
Kyaa-A <asnaripacalna@gmail.com>
2024-12-24 07:59:01 +0800

    Delete Function

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,18 @@
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    console.log("Delete button", item);
+    setTodos(todos.filter((todo) => todo !== item));
+    
+  }
+
   return (
     <div className="item">
       <div className="itemName">
         {item}
         <span>
-          <button className="deleteButton">X</button>
+          <button onClick={() => handleDelete(item)} className="deleteButton">
+            X
+          </button>
         </span>
       </div>
     </div>
