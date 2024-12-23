0381cf99c80d137b00e0b7cbc4126eab98f72079
Kyaa-A <asnaripacalna@gmail.com>
2024-12-23 23:50:20 +0800

    Delete Button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,12 @@
 export default function TodoItem({ item }) {
   return (
     <div className="item">
-      <div className="itemName">{item}</div>
+      <div className="itemName">
+        {item}
+        <span>
+          <button className="deleteButton">Delete</button>
+        </span>
+      </div>
     </div>
   );
 }
